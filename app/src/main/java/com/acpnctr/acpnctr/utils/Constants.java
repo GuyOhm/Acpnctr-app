bb7842a4635495e9b24479782da2c085a23b0f90
GuyOhm <myw4tch@gmail.com>
2017-11-21 21:49:28 +0100

    added constant string for users collection

--- app/src/main/java/com/acpnctr/acpnctr/utils/Constants.java
+++ app/src/main/java/com/acpnctr/acpnctr/utils/Constants.java
@@ -1,10 +1,15 @@
 package com.acpnctr.acpnctr.utils;
 
 /**
- * Created by guiguette on 28/09/2017.
+ * This class contains constants that we use through the app
  */
 
 public class Constants {
+
+    /**
+     * Firestore constants used to identify db nodes i.e. Collection and Document
+     */
+    public static final String FIRESTORE_COLLECTION_USERS = "users";
 
     // Node names in Firebase database
     public static final String FIREBASE_NODE_CLIENT_INFO = "clientInfo";
