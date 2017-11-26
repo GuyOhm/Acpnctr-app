f86fb93602c0efd355adcc16c9b5781e05bf4a38
GuyOhm <myw4tch@gmail.com>
2017-11-26 19:25:09 +0100

    Added clients collection and Intent keys

--- app/src/main/java/com/acpnctr/acpnctr/utils/Constants.java
+++ app/src/main/java/com/acpnctr/acpnctr/utils/Constants.java
@@ -7,13 +7,32 @@ package com.acpnctr.acpnctr.utils;
 public class Constants {
 
     /**
+     *
      * Firestore constants used to identify db nodes i.e. Collection and Document
      */
     public static final String FIRESTORE_COLLECTION_USERS = "users";
+    public static final String FIRESTORE_COLLECTION_CLIENTS = "clients";
 
-    // Node names in Firebase database
-    public static final String FIREBASE_NODE_CLIENT_INFO = "clientInfo";
+    /**
+     * Constants for extra key passing to intent
+     */
+    public static final String INTENT_EXTRA_UID = "uid";
+    public static final String INTENT_EXTRA_CLIENTID = "clientid";
 
-    // Firebase object properties
-    public static final String FIREBASE_PROPERTY_TIMESTAMP = "timestamp";
+    /**
+     * Possible values for the gender of the client.
+     */
+    public static final String GENDER_UNKNOWN = "unknown";
+    public static final String GENDER_MALE = "male";
+    public static final String GENDER_FEMALE = "female";
+
+    /**
+     * Possible values for the acquisition channel.
+     */
+    public static final String ACQUI_UNKNOWN = "unknown";
+    public static final String ACQUI_WOM = "word of mouth";
+    public static final String ACQUI_WEBSITE = "website";
+    public static final String ACQUI_FACEBOOK = "facebook";
+    public static final String ACQUI_CONFRERE = "confrere";
+    public static final String ACQUI_OFFLINE = "offline";
 }
