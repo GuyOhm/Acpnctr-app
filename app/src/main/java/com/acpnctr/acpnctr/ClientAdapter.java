5df9a00d382239e3e9fd87b2a219d3fad46bac00
GuyOhm <myw4tch@gmail.com>
2017-12-02 15:12:43 +0100

    Added a custom Adapter for clients list display  - extends from FirestoreRecyclerAdapter (FirebaseUI)  - OnClickHandler interface to allow to pick an item amongst the list

---
+++ app/src/main/java/com/acpnctr/acpnctr/ClientAdapter.java
@@ -0,0 +1,89 @@
+package com.acpnctr.acpnctr;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.acpnctr.acpnctr.models.Client;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+/**
+ * Adapter to feed data from firestore to the RecyclerView via ViewHoler
+ */
+
+public class ClientAdapter extends FirestoreRecyclerAdapter<Client, ClientAdapter.ClientHolder> {
+
+    // interface to manage how client are selected
+    public interface OnClientSelectedListener {
+        void onClientSelected(Client client);
+    }
+
+    private OnClientSelectedListener mListener;
+
+    /**
+     * Create a new RecyclerView adapter that listens to a Firestore Query.  See
+     * {@link FirestoreRecyclerOptions} for configuration options.
+     *
+     * @param options
+     * @param listener
+     */
+    public ClientAdapter(FirestoreRecyclerOptions<Client> options, OnClientSelectedListener listener) {
+        super(options);
+        mListener = listener;
+    }
+
+    @Override
+    public ClientHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new instance of the ViewHolder using our custom layout for each item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.client_list_item, viewGroup, false);
+
+        return new ClientHolder(view);
+    }
+
+    @Override
+    protected void onBindViewHolder(ClientHolder holder, int position, Client model) {
+        // bind the Client object to the ClientHolder
+        holder.bind(model, mListener);
+    }
+
+    static class ClientHolder extends RecyclerView.ViewHolder {
+
+        // display the client's name
+        private TextView listItemClientName;
+
+        // display the timestampCreated for the client file
+        // TODO: replace this by the last session data
+        private TextView listItemTimestampCreated;
+
+        /**
+         * Constructor for our ViewHolder. Within this constructor, we get a reference to our
+         * TextViews.
+         * @param itemView The View that we inflate in onCreateViewHolder(ViewGroup, int) when creating
+         *                 the FirestoreRecyclerAdapter in {@link DashboardActivity}
+         */
+        public ClientHolder(View itemView) {
+            super(itemView);
+
+            listItemClientName = itemView.findViewById(R.id.tv_client_list_name);
+            listItemTimestampCreated = itemView.findViewById(R.id.tv_client_list_last_session);
+        }
+
+        public void bind(final Client client, final OnClientSelectedListener listener){
+            listItemClientName.setText(client.getClientName());
+            listItemTimestampCreated.setText(String.valueOf(client.getTimestampCreated()));
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listener != null) {
+                        listener.onClientSelected(client);
+                    }
+                }
+            });
+        }
+    }
+}
